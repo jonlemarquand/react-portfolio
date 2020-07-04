d2929877fd2c4b84c47362b2a289ac1fd7e181b8
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-07-04 17:54:33 +0100

    Blog images

--- src/components/blog/BlogSummary.js
+++ src/components/blog/BlogSummary.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import './BlogSummary.scss';
 
-const BlogSummary = ({ blogTitle, blogCategory, children, blogDate }) => {
+const BlogSummary = ({ blogTitle, blogCategory, children, blogDate, blogImg }) => {
     return (
         <div className="blog-summary">
-            <img className="blog-image" src={require(`../../assets/images/icecream.jpg`)} alt="git tragedy" />
+            <img className="blog-image" src={require(`../../assets/images/${blogImg}`)} alt={blogTitle} />
             <div className="blog-text">
                 <h2>{blogTitle}</h2>
                 <div className="blog-category">{blogDate} | {blogCategory}</div>
--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -8,9 +8,9 @@ const Blog = () => {
     return (
         <Layout>
             <h1>Blog</h1>
-            <BlogSummary blogTitle="What a Git" blogDate="2 July 2020" blogCategory="Git, React"><p>Excitedly onboarding to an ongoing project, working with React, Less and all my favourite things. What could possible go wrong?</p></BlogSummary>
-            <BlogSummary blogTitle="SQL: The Prequel" blogDate="24 June 2020" blogCategory="SQL"><p>Databases can make for a good headline when hacked, but often don't have the same level of thought put in by developers. That was the same for me, until now.</p></BlogSummary>
-            <BlogSummary blogTitle="Get Your Own Game" blogDate="10 June 2020" blogCategory="React"><p>As a regular visitor to the BBC Sport website, I noticed they were using React to create their quizzes and thought to myself, 'How hard could it be?'</p></BlogSummary>
+            <BlogSummary blogTitle="What a Git" blogDate="2 July 2020" blogCategory="Git, React" blogImg="blog-git.png"><p>Excitedly onboarding to an ongoing project, working with React, Less and all my favourite things. What could possible go wrong?</p></BlogSummary>
+            <BlogSummary blogTitle="SQL: The Prequel" blogDate="24 June 2020" blogCategory="SQL" blogImg="blog-sqlprequel.png"><p>Databases can make for a good headline when hacked, but often don't have the same level of thought put in by developers. That was the same for me, until now.</p></BlogSummary>
+            <BlogSummary blogTitle="The Beautiful Game" blogDate="10 June 2020" blogCategory="React" blogImg="blog-beautifulgame.png"><p>As a regular visitor to the BBC Sport website, I noticed they were using React to create their quizzes and thought to myself, 'How hard could it be?'</p></BlogSummary>
         </Layout>
     )
 }
